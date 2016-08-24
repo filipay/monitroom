f48228b43e6d6d9a4055908a31a6fc2359f14969
Filip Piskor <filipay@hotmail.com>
2016-08-25 00:09:08 +0100

    DB no longer handles caching. Metrics do. Added /live and /all to /scan

--- controllers/datastores.js
+++ controllers/datastores.js
@@ -14,42 +14,26 @@ db.devices.ensureIndex({ fieldName: 'mac_addr', unique: true }, function (err) {
 });
 
 //Cache latest info
-db.devices._cache = {};
 
 db.devices.fetchDevices = function (callback) {
 
   db.devices.find({}, function (err, docs) {
     //TODO log as error instead of just throwing it
     if (err) throw err;
-
-    docs.forEach(function (doc) {
-      if (!db.devices._cache[doc.mac_addr]) {
-        db.devices._cache[doc.mac_addr] = new Device(doc);
-      }
-    });
-    console.log(db.devices._cache);
     if (callback) callback(docs);
   });
 
 };
 
 db.devices.updateDevices = function (devices, callback) {
-  var meaningful_devices = [];
-  devices.forEach(function (device) {
-    //Check if device is already cached, if not create new device
-    var currDevice = db.devices._cache[device.mac] || new Device(device);
-    currDevice.updateTime(device.timestamp);
-    db.devices._cache[device.mac] = currDevice;
-    meaningful_devices.push(currDevice.__self__());
 
-    db.devices.update({ mac_addr: device.mac }, currDevice.__self__(), { upsert: true }, function (err) {
+  devices.forEach(function (device) {
+    db.devices.update({ mac_addr: device.mac_addr }, device, { upsert: true }, function (err) {
       if (err) throw err;
     });
   });
 
-  if (callback) callback(meaningful_devices);
-  console.log(db.devices._cache);
-
+  if (callback) callback(devices);
 };
 
 
