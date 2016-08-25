f7429523fcd2438db59361c56005da31ce04552d
Filip Piskor <filipay@hotmail.com>
2016-08-25 21:18:41 +0100

    Added Winston logger. Moved classes around.

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var metrics = require('../controllers/metrics');
-var db = require('../controllers/datastores');
-var watch = require('../controllers/watch')();
 
-watch.setCheckRate(5);
+var metrics = require('../controllers/metrics');
+var watch = require('../controllers/watch')();
+var db = require('../helper/datastores');
+var logger = require('../helper/logger');
+
 
 //GET showing only online devices
 router.get('/scan/live', function(req, res, next) {
   metrics.networkScan(function (devices) {
     db.devices.updateDevices(devices);
+    logger.info('GET /scan/live', devices.map(function (device) {
+      return device.mac_addr;
+    }));
     res.send(devices);
   });
 });
 
 //GET shows all devices, both online
-router.get('/scan/all', function (req, res) {
+router.get('/scan', function (req, res) {
 
   //TODO make sure the latest data merges with the old data
-  db.devices.fetchDevices(function (data) {
+  db.devices.fetchDevices(function (devices) {
+    logger.info('GET /scan/live', devices.map(function (device) {
+      return device.mac_addr;
+    }));
     res.send(data);
   });
 });
 
 router.put('/scan', function (req, res) {
-  console.log(req.body());
+  db.devices.update('')
   res.send();
 });
 
