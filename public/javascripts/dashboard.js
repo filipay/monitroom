ce701736d68831cae918004cfe66a9fe30252b70
Filip Piskor <filipay@hotmail.com>
2016-08-29 21:42:14 +0100

    Work started on dashboard. Live results display by default. Small tweaks at metrics. Disabled watch in /routes/api.js

---
+++ public/javascripts/dashboard.js
@@ -0,0 +1,31 @@
+
+  var app = {
+    get : {
+      devices: function () {
+        $.get('http://localhost:3000/api/scan/live', function (results) {
+          var td = $('.td-template').clone();
+          var tr = $('.tr-template').empty().clone();
+          var index = 0;
+          $('.tbody-template').empty();
+
+          results.forEach(function (device) {
+            var device_tr = tr.clone();
+
+            device_tr.append(td.clone().html(index));
+            device_tr.append(td.clone().html(device.name));
+            device_tr.append(td.clone().html(device.ip));
+            device_tr.append(td.clone().html(device.mac_addr));
+            device_tr.append(td.clone().html(device.vendor));
+            var last_seen = Math.floor((Date.now() - device.times.end) / (60 * 1000));
+            device_tr.append(td.clone().html(last_seen + ' minutes ago'));
+
+            $('.tbody-template').append(device_tr);
+            index++;
+          });
+
+        });
+      }
+    }
+  };
+
+  $(app.get.devices);
