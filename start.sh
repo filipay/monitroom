e69c0350ed7dc9d67ab3bf8f3839a7d29f0656fa
SHANE <shane.carty@hotmail.com>
2016-10-01 00:04:31 +0100

    updates start script to create log file if it doesn't exist

--- start.sh
+++ start.sh
@@ -1,1 +1,15 @@
+# checks if logs folder exists, if not creates it and the log file
+  
+# gets the directory of bash script (incase it was run from another dir)  
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/logs"  
+LOG_FILE="$DIR/monitroom-info.log" 
+
+if [ ! -d $DIR ]; then
+  echo "creating log file..."
+  
+  echo $LOG_FILE
+  mkdir $DIR 
+  touch $LOG_FILE
+
+fi
 sudo DEBUG=monitroom:* npm start
